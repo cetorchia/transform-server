643e53dcdd7c7904cee976783ecfbc8aa3baae9c
Carlos Torchia <ctorchia87@gmail.com>
2015-12-21 21:16:42 -0800

    GPL each erlang file

--- src/user_data.hrl
+++ src/user_data.hrl
@@ -1,2 +1,18 @@
+%% This file is part of the transform server
+%% Copyright (c) 2015 Carlos E. Torchia
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 -record(user_data, {id, user_profile_id, data_collection_id, key, updated, data}).
 -record(user_data_key, {data_collection_id, key}).
